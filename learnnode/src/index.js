ac2014c5a07ac968a3f57290520b12057680bd70
Moosest <martenpoder@pm.me>
2026-03-16 17:20:31 +0200

    add buttons element

--- learnnode/src/index.js
+++ learnnode/src/index.js
@@ -5,11 +5,13 @@ import { createWebHashHistory, createWebHistory, createRouter } from 'vue-router
 import ToDo from "./Pages/ToDo.vue";
 import Modals from "./Pages/Modals.vue";
 import Messages from "./Pages/Messages.vue"
+import Buttons from "./Pages/Buttons.vue"
 
 const routes = [
     { path: '/', component: ToDo, name: 'ToDo' },
     { path: '/modals', component: Modals, name: 'Modals' },
     { path: '/messages', component: Messages, name: 'Messages' },
+    { path: '/buttons', component: Buttons, name: 'Buttons' },
 ]
 
 export const router = createRouter({
