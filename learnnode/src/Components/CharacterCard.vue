0a0a80866a14af61cf5f0dfd2027677d46e6b7bb
Moosest <martenpoder@pm.me>
2026-05-13 10:48:08 +0300

    added filter via status and gender

--- learnnode/src/Components/CharacterCard.vue
+++ learnnode/src/Components/CharacterCard.vue
@@ -5,7 +5,9 @@ defineProps(['character'])
 <template>
     <div class="card">
         <header class="card-header">
-            <p class="card-header-title">{{ character.name }}</p>
+            <p class="card-header-title">
+                {{ character.name }}
+            </p>
         </header>
         <div class="card-image">
             <figure class="image is-4by3">
